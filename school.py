2a13a55525173290645fce4bdd87351ac6be3433
arsan-che <arsan903@gmail.com>
2025-02-17 14:22:37 +0000

    added a function and print messages

--- school.py
+++ school.py
@@ -15,3 +15,15 @@ class School:
 #added setters
   def setNumberOfStudents(self, new_number):
     self.numberOfStudents = new_number
+    
+    
+def __repr__(self):
+    return 'A {level} school named {name} with {numberOfStudents} students.'.format(level = self.level, name = self.name, numberOfStudents = self.numberOfStudents)
+
+a = School("Codecademy", "high", 100)
+print(a)
+print(a.getName())
+print(a.getLevel())
+a.setNumberOfStudents(200)
+print(a.getNumberOfStudents())
+#defined PrimarySchool class inheriting from School class with 3 attributes and 1 method
